23a299d3938f0a1e0ca1d3857485b364989ae667
jhansi <suvindyajanu125@gmail.com>
2023-03-19 19:24:59 +0530

    leftTriangle.java

--- leftTriangle.java
+++ leftTriangle.java
@@ -1,32 +1,26 @@
-class Solution 
-{ 
- public void pattern()
- {
-   int n = 4;
-   for(int i=0;i<n;i++)
-   {
-    //  System.out.println();
-     for(int j=0;j<n-i;j++)
-     {
-       System.out.print(" ");
-     }
-    //  int c=1;
-     for(int k=0;k<=i;k++)
-     {
-       System.out.print("*");
-      //  System.out.print(" ");
-      //  c = c*(i - k) /k;  
-     }
-     System.out.println();
-   }
- }
+class Solution1 {
+  public void pattern() {
+    int n = 4;
+    for (int i = 0; i < n; i++) {
+      // System.out.println();
+      for (int j = 0; j < n - i; j++) {
+        System.out.print(" ");
+      }
+      // int c=1;
+      for (int k = 0; k <= i; k++) {
+        System.out.print("*");
+        // System.out.print(" ");
+        // c = c*(i - k) /k;
+      }
+      System.out.println();
+    }
+  }
 }
 
-class leftTriangle
-{
-  public static void main(String args[])
-  {
-    Solution obj = new Solution();
+class LeftTriangle {
+  public static void main(String args[]) {
+    Solution1 obj = new Solution1();
     obj.pattern();
   }
+
 }
